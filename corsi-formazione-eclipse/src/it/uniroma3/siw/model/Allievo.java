a4873feb504e452bb2a1abe2cfbe091510eeee1b
Anahita Yeganian <anahitayeganian@gmail.com>
2022-04-20 02:08:21 +0200

    Implemented getter and setter methods plus relationships mapping for Allievo and Societa classes.

--- corsi-formazione-eclipse/src/it/uniroma3/siw/model/Allievo.java
+++ corsi-formazione-eclipse/src/it/uniroma3/siw/model/Allievo.java
@@ -1,5 +1,8 @@
 package it.uniroma3.siw.model;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,13 +11,95 @@ import javax.persistence.ManyToOne;
 
 @Entity
 public class Allievo {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
+	@Column(nullable = false)
+	private String matricola;
+
+	@Column(nullable = false)
+	private String cognome;
+
+	@Column(nullable = false)
+	private String nome;
+
+	private LocalDate dataNascita;
+
+	private String luogoNascita;
+
+	@Column(nullable = false)
+	private String email;
+
 	/* Allievo parte proprietaria */
 	@ManyToOne
-	private Corso corsoh;
+	private Corso corso;
 
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMatricola() {
+		return this.matricola;
+	}
+
+	public void setMatricola(String matricola) {
+		this.matricola = matricola;
+	}
+
+	public String getCognome() {
+		return this.cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascita() {
+		return this.dataNascita;
+	}
+
+	public void setDataNascita(LocalDate dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public String getLuogoNascita() {
+		return this.luogoNascita;
+	}
+
+	public void setLuogoNascita(String luogoNascita) {
+		this.luogoNascita = luogoNascita;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		return this.getId().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Allievo allievo = (Allievo)obj;
+		return this.getId().equals(allievo.getId());
+	}
 }
--- corsi-formazione-eclipse/src/it/uniroma3/siw/model/Societa.java
+++ corsi-formazione-eclipse/src/it/uniroma3/siw/model/Societa.java
@@ -2,6 +2,7 @@ package it.uniroma3.siw.model;
 
 import java.util.List;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -16,7 +17,49 @@ public class Societa {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
+	@Column(nullable = false)
+	private String ragioneSociale;
+	
+	private String numeroTelefono;
+	
 	@OneToMany
 	@JoinColumn(name = "societa_id") // Forzo la FK e non la join table
 	private List<Allievo> dipendenti;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getRagioneSociale() {
+		return this.ragioneSociale;
+	}
+
+	public void setRagioneSociale(String ragioneSociale) {
+		this.ragioneSociale = ragioneSociale;
+	}
+
+	public String getNumeroTelefono() {
+		return this.numeroTelefono;
+	}
+
+	public void setNumeroTelefono(String numeroTelefono) {
+		this.numeroTelefono = numeroTelefono;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.getId().hashCode();
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		Societa societa = (Societa)obj;
+		return this.getId().equals(societa.getId());
+	}
+	
+	
 }
